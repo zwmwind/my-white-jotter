a3ba2d344040159b7313b39e146dc5b47870b6ee
MyOMEN <zhangweiming1995@qq.com>
2021-12-25 02:44:22 +0800

    first commit

---
+++ src/main/java/com/zwm/mywhitejotter/MyWhiteJotterApplication.java
@@ -0,0 +1,13 @@
+package com.zwm.mywhitejotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWhiteJotterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWhiteJotterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zwm/mywhitejotter/MyWhiteJotterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwm.mywhitejotter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWhiteJotterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
